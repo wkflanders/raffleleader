13888c8ecd96766076c6b26a94eeb49562541a09
William Flanders <wflan@seas.upenn.edu>
2024-10-23 19:32:44 -0400

    tool switching

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
+
+import { ActiveTool } from "@/features/editor/types";
 
 import { useEditor } from "@/features/editor/hooks/use-editor";
 import { Navbar } from "@/features/editor/components/navbar";
@@ -10,6 +12,24 @@ import { Toolbar } from "@/features/editor/components/toolbar";
 import { Footer } from "@/features/editor/components/footer";
 
 export const Editor = () => {
+  const [activeTool, setActiveTool] = useState<ActiveTool>("select");
+
+  const onChangeActiveTool = useCallback((tool: ActiveTool) => {
+    if(tool === activeTool){
+      return setActiveTool("select");
+    }
+
+    if(tool === "draw"){
+      // TODO: Enable draw mode
+    }
+
+    if(activeTool === "draw"){
+      // TODO: Disable draw mode
+    }
+
+    setActiveTool(tool);
+  }, [activeTool]);
+
   const { init } = useEditor();
 
   const canvasRef = useRef(null);
@@ -33,9 +53,15 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <Navbar />
+      <Navbar 
+        activeTool={activeTool}
+        onChangeActiveTool={onChangeActiveTool}
+      />
       <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
-        <Sidebar />
+        <Sidebar 
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar />
           <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
