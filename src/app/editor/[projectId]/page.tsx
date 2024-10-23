ea7c72d99b8a6c3159628ea007e04a72c68c0b05
William Flanders <wflan@seas.upenn.edu>
2024-10-23 12:00:28 -0400

    editor route

---
+++ src/app/editor/[projectId]/page.tsx
@@ -0,0 +1,11 @@
+import { Editor } from "@/features/editor/components/editor";
+
+const EditorProjectIdPage = () => {
+    return ( 
+        <Editor>
+            
+        </Editor>
+     );
+};
+ 
+export default EditorProjectIdPage;
