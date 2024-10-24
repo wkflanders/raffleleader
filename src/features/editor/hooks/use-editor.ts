54f39e0e0a902b28de973f31b5e7889e20db1fa3
William Flanders <wflan@seas.upenn.edu>
2024-10-24 02:58:28 -0400

    layers functionality

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -52,6 +52,26 @@ const buildEditor = ({
     };
 
     return {
+        bringForward: () => {
+            canvas.getActiveObjects().forEach((object) => {
+                canvas.bringForward(object);
+            });
+
+            canvas.renderAll();
+            
+            const workspace = getWorkspace();
+            workspace?.sendToBack();
+        },
+        sendBackwards: () => {
+            canvas.getActiveObjects().forEach((object) => {
+                canvas.sendBackwards(object);
+            });
+
+            canvas.renderAll();
+            
+            const workspace = getWorkspace();
+            workspace?.sendToBack();
+        },
         changeFillColor: (value: string) => {
             setFillColor(value);
             canvas.getActiveObjects().forEach((object) => {
