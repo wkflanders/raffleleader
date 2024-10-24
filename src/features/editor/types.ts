03754d7799937dd2c322459353f3f318b254a090
William Flanders <wflan@seas.upenn.edu>
2024-10-24 02:58:11 -0400

    layering typing

--- src/features/editor/types.ts
+++ src/features/editor/types.ts
@@ -123,6 +123,8 @@ export type BuildEditorProps = {
 };
 
 export interface Editor {
+  bringForward: () => void;
+  sendBackwards: () => void;
   changeFillColor: (value: string) => void;
   changeStrokeColor: (value: string) => void;
   changeStrokeWidth: (value: number) => void;
