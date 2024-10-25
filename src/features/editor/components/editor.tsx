d4bd2895e7d0430ae4462eb444667404cdfa3b94
William Flanders <wflan@seas.upenn.edu>
2024-10-25 11:40:26 -0400

    font sidebar menu

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -16,6 +16,7 @@ import { StrokeColorSidebar } from "@/features/editor/components/stroke-color-si
 import { StrokeWidthSidebar } from "@/features/editor/components/stroke-width-sidebar";
 import { OpacitySidebar } from "@/features/editor/components/opacity-sidebar";
 import { TextSidebar } from "@/features/editor/components/text-sidebar";
+import { FontSidebar } from "@/features/editor/components/font-sidebar";
 
 
 export const Editor = () => {
@@ -103,6 +104,11 @@ export const Editor = () => {
           onChangeActiveTool={onChangeActiveTool}
         />
         <TextSidebar
+          editor={editor}
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
+        <FontSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
