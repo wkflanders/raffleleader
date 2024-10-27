5ac98218b803543ae2cac25d10e62dfa8b50865f
William Flanders <wflan@seas.upenn.edu>
2024-10-27 00:31:59 -0400

    image sidebar menu

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -17,6 +17,7 @@ import { StrokeWidthSidebar } from "@/features/editor/components/stroke-width-si
 import { OpacitySidebar } from "@/features/editor/components/opacity-sidebar";
 import { TextSidebar } from "@/features/editor/components/text-sidebar";
 import { FontSidebar } from "@/features/editor/components/font-sidebar";
+import { ImageSidebar } from "@/features/editor/components/image-sidebar";
 
 
 export const Editor = () => {
@@ -109,6 +110,11 @@ export const Editor = () => {
           onChangeActiveTool={onChangeActiveTool}
         />
         <FontSidebar
+          editor={editor}
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
+        <ImageSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
