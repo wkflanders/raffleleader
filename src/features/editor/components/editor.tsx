0186aab16f11495205062034677715e521a66211
William Flanders <wflan@seas.upenn.edu>
2024-10-28 00:18:27 -0400

    ai sidebar menu

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -19,7 +19,7 @@ import { TextSidebar } from "@/features/editor/components/text-sidebar";
 import { FontSidebar } from "@/features/editor/components/font-sidebar";
 import { ImageSidebar } from "@/features/editor/components/image-sidebar";
 import { FilterSidebar } from "@/features/editor/components/filter-sidebar";
-
+import { AiSidebar } from "@/features/editor/components/ai-sidebar";
 
 export const Editor = () => {
   const [activeTool, setActiveTool] = useState<ActiveTool>("select");
@@ -121,6 +121,11 @@ export const Editor = () => {
           onChangeActiveTool={onChangeActiveTool}
         />
         <FilterSidebar
+          editor={editor}
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
+        <AiSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
