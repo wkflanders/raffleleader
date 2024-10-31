d22f9aa8c556628feb24604e57147da32a7c5497
William Flanders <wflan@seas.upenn.edu>
2024-10-31 01:00:16 -0400

    draw mode sidebar

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -21,25 +21,10 @@ import { ImageSidebar } from "@/features/editor/components/image-sidebar";
 import { FilterSidebar } from "@/features/editor/components/filter-sidebar";
 import { AiSidebar } from "@/features/editor/components/ai-sidebar";
 import { RemoveBgSidebar } from "@/features/editor/components/remove-bg-sidebar";
+import { DrawSidebar } from "@/features/editor/components/draw-sidebar";
 
 export const Editor = () => {
   const [activeTool, setActiveTool] = useState<ActiveTool>("select");
-
-  const onChangeActiveTool = useCallback((tool: ActiveTool) => {
-    if (tool === activeTool) {
-      return setActiveTool("select");
-    }
-
-    if (tool === "draw") {
-      // TODO: Enable draw mode
-    }
-
-    if (activeTool === "draw") {
-      // TODO: Disable draw mode
-    }
-
-    setActiveTool(tool);
-  }, [activeTool]);
 
   const onClearSelection = useCallback(() => {
     if (selectionDependentTools.includes(activeTool)) {
@@ -50,6 +35,23 @@ export const Editor = () => {
   const { init, editor } = useEditor({
     clearSelectionCallback: onClearSelection,
   });
+
+  const onChangeActiveTool = useCallback((tool: ActiveTool) => {
+    if (tool === "draw") {
+      editor?.enableDrawingMode();
+    }
+
+    if (activeTool === "draw") {
+      editor?.disableDrawingMode();
+    }
+
+    if (tool === activeTool) {
+      return setActiveTool("select");
+    }
+
+    setActiveTool(tool);
+  }, [activeTool, editor]);
+
 
   const canvasRef = useRef(null);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -132,6 +134,11 @@ export const Editor = () => {
           onChangeActiveTool={onChangeActiveTool}
         />
         <RemoveBgSidebar
+          editor={editor}
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
+        <DrawSidebar
           editor={editor}
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
