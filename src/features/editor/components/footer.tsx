7eeba9b05a0bc7ff3a904e79f0923b7367da6828
William Flanders <wflan@seas.upenn.edu>
2024-10-31 02:35:26 -0400

    zoom in/out/reset buttons

--- src/features/editor/components/footer.tsx
+++ src/features/editor/components/footer.tsx
@@ -1,7 +1,50 @@
-export const Footer = () => {
+import { Editor } from "@/features/editor/types";
+
+import { Minimize, ZoomIn, ZoomOut } from "lucide-react";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+
+
+interface FooterProps {
+    editor: Editor | undefined;
+}
+
+export const Footer = ({
+    editor,
+}: FooterProps) => {
     return (
         <footer className="h-[52px] border-t bg-white w-full flex items-center overflow-x-auto z-[49] p-2 gap-x-1 shrink-0 px-4 flex-row-reverse">
-
+            <Hint label="Reset" side="top" sideOffset={10}>
+                <Button
+                    onClick={() => editor?.autoZoom()}
+                    size="icon"
+                    variant="ghost"
+                    className="h-full"
+                >
+                    <Minimize className="size-4"/>
+                </Button>
+            </Hint>
+            <Hint label="Zoom In" side="top" sideOffset={10}>
+                <Button
+                    onClick={() => editor?.zoomIn()}
+                    size="icon"
+                    variant="ghost"
+                    className="h-full"
+                >
+                    <ZoomIn className="size-4"/>
+                </Button>
+            </Hint>
+            <Hint label="Zoom In" side="top" sideOffset={10}>
+                <Button
+                    onClick={() => editor?.zoomOut()}
+                    size="icon"
+                    variant="ghost"
+                    className="h-full"
+                >
+                    <ZoomOut className="size-4"/>
+                </Button>
+            </Hint>
         </footer>
     );
 }
