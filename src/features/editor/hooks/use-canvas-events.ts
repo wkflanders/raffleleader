5536524ffffb504fbdd92ae7708e344285217e60
William Flanders <wflan@seas.upenn.edu>
2024-10-31 12:40:10 -0400

    save canvas events to history

--- src/features/editor/hooks/use-canvas-events.ts
+++ src/features/editor/hooks/use-canvas-events.ts
@@ -3,17 +3,22 @@ import { useEffect } from "react";
 
 interface UseCanvasEventsProps {
     canvas: fabric.Canvas | null;
+    save: () => void;
     setSelectedObjects: (objects: fabric.Object[]) => void;
     clearSelectionCallback?: () => void;
 }
 
 export const useCanvasEvents = ({
     canvas,
+    save,
     setSelectedObjects,
     clearSelectionCallback,
 }: UseCanvasEventsProps) => {
     useEffect(() => {
         if (canvas) {
+            canvas.on("object:added", () => save());
+            canvas.on("object:removed", () => save());
+            canvas.on("object:modified", () => save());
             canvas.on("selection:created", (e) => {
                 setSelectedObjects(e.selected || []);
             });
@@ -28,6 +33,9 @@ export const useCanvasEvents = ({
 
         return () => {
             if(canvas) {
+                canvas.off("object:added");
+                canvas.off("object:removed");
+                canvas.off("object:modified");
                 canvas.off("selection:created");
                 canvas.off("selection:updated");
                 canvas.off("selection:cleared");
@@ -35,6 +43,7 @@ export const useCanvasEvents = ({
         }
     }, [
         canvas,
+        save,
         clearSelectionCallback,
         setSelectedObjects, // No need other than to make linter happy since comes from setState
     ]);
