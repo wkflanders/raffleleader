af19fa2f0a9ff5695092d12587d5d2c2a3a0b96b
William Flanders <wflan@seas.upenn.edu>
2024-10-31 01:00:22 -0400

    draw mode sidebar

--- src/features/editor/components/sidebar.tsx
+++ src/features/editor/components/sidebar.tsx
@@ -4,7 +4,6 @@ import {
   LayoutTemplate,
   ImageIcon,
   Pencil,
-  Presentation,
   Settings,
   Shapes,
   Sparkles,
@@ -45,6 +44,12 @@ export const Sidebar = ({ activeTool, onChangeActiveTool }: SidebarProps) => {
           label="Shapes"
           isActive={activeTool === "shapes"}
           onClick={() => onChangeActiveTool("shapes")}
+        />
+        <SidebarItem
+          icon={Pencil}
+          label="Draw"
+          isActive={activeTool === "draw"}
+          onClick={() => onChangeActiveTool("draw")}
         />
         <SidebarItem
           icon={Sparkles}
